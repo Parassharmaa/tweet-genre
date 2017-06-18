6c6b29218c0c0d103704ebb9a02e2ecffdf5abc5
parassharmaa <paraazz@outlook.com>
2017-06-19 01:30:38 +0530

    [UPDATE]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import _ from "lodash";
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,38 +11,92 @@ import { Http } from '@angular/http';
 })
 
 export class AppComponent {
-  title = 'app';
+  title = 'Tweet2Genre';
+  sub = "Analyse your tweet's topic!";
   showId : boolean;
-  myData : any;
+  gData : any;
   username : string;
+  showResult : boolean;
+  error : string;
+  showError : boolean;
+  selectedOption: string;
+  windowH : any;
+  windowW : any;
+
+  public pie_ChartOptions;
+
+  public gauge_ChartOptions;
+
+  pie_ChartData  = [["Topic", "Values"]];
 
   constructor(private http: Http) {
     this.username = "";
     this.showId = false;
+    this.showResult  = false;
+    this.error = "";
+    this.showError  = false;
+
+    if (window.innerWidth < 600) {
+      this.windowH = 550
+      this.windowW = window.innerWidth;
+    } else {
+      this.windowH = 550
+      this.windowW = 700
+    }
+
+    this.pie_ChartOptions = {
+        title: 'Tweets Genre',
+        width: this.windowW-50,
+        height:this.windowH
+    };
+
+    this.gauge_ChartOptions = {
+        width: this.windowW-100, height: this.windowH-100,
+        redFrom: 90, redTo: 100,
+        yellowFrom: 75, yellowTo: 90,
+        minorTicks: 5
+    };
+    console.log(this.windowH, this.windowW);
   }
 
-  toggleLoader() {
-    this.showId = !this.showId;
+
+  public gauge_ChartData = [['Label', 'Value']];
+
+  toggleError(e: string) {
+    this.error = e;
+    this.showError = true;
   }
 
   onKey(event: any) {
-    if(event.keyCode == 13) {
+    console.log(event)
+    console.log(event)
+    if(event.keyCode == 13 && event.target.value !== "") {
+      event.target.blur();
+      this.pie_ChartData  = [["Topic", "Values"]];
+      this.gauge_ChartData = [
+        ['Label', 'Value']];
       this.username = event.target.value;
-      this.toggleLoader();
+      this.showId = true;
+      this.showError = false;
+      this.showResult = false;
       this.http.get('https://twg-api.herokuapp.com/api/genre?username='+this.username)
-      .map(response => response.json())
-      .subscribe(res => {
-        this.myData = JSON.stringify(res);
-        this.toggleLoader();
-        console.log(this.myData)
-      });
+        .map(response => response.json())
+        .subscribe(res => {
+          this.gData = res['categories'];
+          if(this.gData.length > 0) {
+            for (var i in this.gData) {
+              this.pie_ChartData.push([this.gData[i].topic, this.gData[i].value]);
+            }
+            this.gauge_ChartData.push([_.maxBy(this.gData, 'value').topic, _.maxBy(this.gData, 'value').value])
+            this.gauge_ChartData.push([_.minBy(this.gData, 'value').topic, _.minBy(this.gData, 'value').value])
 
+            this.showResult = true;
+            this.showId = false;
+          } else {
+            this.toggleError("User not found or account is Private");
+            this.showId = false;
+          }
+        });
     }
   }
-
-  // constructor(private https: Http) {
-  //   this.https.get('http://book-bot-ai.herokuapp.com/api/download?hash=CD2CB86351480A1FF50B95F008CC9552')
-  //     .map(response => response.json())
-  //     .subscribe(res => this.myData = res);
-  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
 import {HttpModule} from '@angular/http'
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule,MdInputModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdProgressBarModule } from '@angular/material';
+import { MdButtonModule,
+        MdInputModule,
+        MdCardModule,
+        MdMenuModule,
+        MdToolbarModule,
+        MdIconModule,
+        MdDialogModule,
+        MdProgressBarModule } from '@angular/material';
+
+
+import {GoogleChart} from 'angular2-google-chart/directives/angular2-google-chart.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleChart
   ],
   imports: [
     BrowserModule,
@@ -18,6 +27,7 @@ import { MdButtonModule,MdInputModule, MdCardModule, MdMenuModule, MdToolbarModu
     BrowserAnimationsModule,
     MdButtonModule,
     MdCardModule,
+    MdDialogModule,
     MdMenuModule,
     MdToolbarModule,
     MdIconModule,
